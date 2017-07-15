63a486f6b4e8d6e639de70f90db48ca7edb1cedc
Austin Markwell <amarkwell8@gmail.com>
2017-07-15 16:34:18 -0700

    Submission for the Clean Up The Words challenge
    https://www.codeeval.com/open_challenges/205/

---
+++ cleanUpTheWords/src/Main.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by austinmarkwell on 7/15/17.
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(new FileInputStream(args[0]))
+        );
+        try{
+            String line;
+            while((line = br.readLine())!=null) {
+                String onlyLetters = line.replaceAll("[^A-Za-z]+", " ");
+                onlyLetters = onlyLetters.trim().replaceAll(" +", " ");
+                System.out.println(onlyLetters.toLowerCase());
+            }
+        } finally {
+            br.close();
+        }
+    }
+}
